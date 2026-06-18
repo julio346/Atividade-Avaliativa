e93052d8a4f47fa706caa9f0bbc68e125ac35a98
JULIO ROBERTO PAULINO DE OLIVEIRA <00001114197166sp@aluno.educacao.sp.gov.br>
2026-06-18 11:42:01 -0300

    Texto de inscrição da prova feito

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function cadastro() {
     window.location.href = "cadastro.html";
 }
+
+document.getElementById("loginEnem").addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    
+    window.location.href = "painel.html"; 
+});
